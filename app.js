cdd7e9dc13a20f1756d6d30fe99b86432e6c475c
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-03-09 17:55:31 +0530

    checking

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const apiKey = "a79657c87600401599949581c69c83ba"
 const url = "https://newsapi.org/v2/everything?q=tesla&from=2024-02-09&sortBy=publishedAt&"
 
-window.addEventListener("load", async () => fetchNews("india"));
-function reload(){
-  window.location.reload();
-}
+// window.addEventListener("load", async () => fetchNews("india"));
+// function reload(){
+//   window.location.reload();
+// }
 
 async function fetchNews(query) {
   const response = await fetch(`https://newsapi.org/v2/everything?q=${query}&from=2024-02-09&sortBy=publishedAt&apiKey=${apiKey}`);
